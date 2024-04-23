4c96d5a91fcba6b61bfd267521b1976e8efe92fb
RolandoGL <vlc960157@gmail.com>
2024-04-23 12:59:18 -0600

    feat: create view pokemonPage for render a list of pokemon

---
+++ src/app/modules/pokemon/view/pokemon-page/pokemon-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-pokemon-page',
+  templateUrl: './pokemon-page.component.html',
+  styleUrls: ['./pokemon-page.component.css']
+})
+export class PokemonPageComponent{
+ 
+}
